1f3e72ee86162d043f3c66ba35a5890bcb4f0231
conmart <connor.a.martinelli@gmail.com>
2017-08-22 17:27:13 -0700

    working game with basic requirements

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
+console.log("ready and connected");
+
+let xturn = true;
 // wait for the DOM to finish loading
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
 
+  $('.box').on('click', function() {
+    if ($(this).text() == '') {
+      if (xturn) {
+        $(this).text('x');
+        xturn = false;
+      } else{
+        $(this).text('o');
+        xturn = true;
+      };
+    };
+
+  });
+
+  $('.reset').on('click', function() {
+    location.reload();
+  });
 
 
 
-  
 
 });
